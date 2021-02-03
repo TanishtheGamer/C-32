cfa00afd9f32b1e667cb15fa1cebbb8f6ff6d919
TanishtheGamer <tanishrreddy@gmail.com>
2021-02-02 17:00:41 -0800

    JSON

--- sketch.js
+++ sketch.js
@@ -7,11 +7,12 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var score = 0;
 
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getTime()
 }
 
 function setup(){
@@ -45,18 +46,26 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if(backgroundImg){
+        background(backgroundImg);
+    }
+    noStroke()
+    textSize(35)
+    fill(255)
+    text("score:"+score,width-300,50)
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -86,3 +95,19 @@ function keyPressed(){
        // slingshot.attach(bird.body);
     }
 }
+
+async function getTime(){
+    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Tokyo");
+    var responseJSON = await response.json();
+    var datetime =  responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+
+    if(hour>=6&&hour<=19){
+        bg = "sprites/bg.png"
+    }
+    else{
+        bg = "sprites/bg2.jpg"
+    }
+    backgroundImg = loadImage(bg)
+}
+
